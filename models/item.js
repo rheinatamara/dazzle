79a98d77a3ad9568b3f4b601a3a276f02ca3a70c
rheinatamara <rheina.tamara@outlook.com>
2025-03-04 12:49:43 +0700

    Configurations

---
+++ models/item.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Item extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Item.init({
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    price: DataTypes.INTEGER,
+    stock: DataTypes.INTEGER,
+    photoUrl: DataTypes.STRING,
+    UserId: DataTypes.INTEGER,
+    CategoryId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Item',
+  });
+  return Item;
+};
