772fcff5daee8e8a43b429d714ed97bef94b65ba
rheinatamara <rheina.tamara@outlook.com>
2025-03-05 00:06:14 +0700

    migration

--- models/favorite.js
+++ models/favorite.js
@@ -2,11 +2,7 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Favorite extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
       Favorite.belongsTo(models.User, { foreignKey: "UserId" });
       Favorite.belongsTo(models.Item, { foreignKey: "ItemId" });
