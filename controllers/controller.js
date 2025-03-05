d5638a2117ed5b23aee01e80a0c6dfc360d1918d
rheinatamara <rheina.tamara@outlook.com>
2025-03-05 14:57:44 +0700

    pages in client

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,25 @@
-const { Category } = require("../models");
+const { Category, Item } = require("../models");
 class Controller {
   static async landingPage(req, res) {
     try {
-      let data = await Category.findAll();
-      res.render("landingPage", { data });
+      const items = await Item.findAll({
+        limit: 3,
+        order: [["createdAt", "DESC"]],
+      });
+      const data = await Category.findAll();
+
+      res.render("landingPage", { data, items });
+    } catch (error) {
+      res.send(error);
+    }
+  }
+  static async categoryItems(req, res) {
+    try {
+      let { id } = req.params;
+      let data = await Category.findByPk(+id, {
+        include: Item,
+      });
+      res.render("category", { data });
     } catch (error) {
       res.send(error);
     }
